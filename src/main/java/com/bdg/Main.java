c702b7737dbe74fa1897d409753b47c081c8bce1
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-03-18 00:17:10 +0400

    Updated project

--- src/main/java/com/bdg/Main.java
+++ src/main/java/com/bdg/Main.java
@@ -1,5 +1,18 @@
-package com.bdg;public class Main {
+package com.bdg;
+
+import org.example.Algorithms1;
+import org.example.ArraysToTest;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CustomArrays customArrays = new CustomArrays();
+        System.out.println(customArrays.maxElementOf(new int[]{1, 2, 3, 4}));  //custom_arrays (my project)
+
+        ArraysToTest toTest = new ArraysToTest(); //arrays_to_test (Angin's project)
+        toTest.desArr();
+        toTest.reverseArr();
+
+        Algorithms1 algorithms1 = new Algorithms1(); //algorithm (Angin's project)
+        algorithms1.largestPrimeFactor();
     }
 }
